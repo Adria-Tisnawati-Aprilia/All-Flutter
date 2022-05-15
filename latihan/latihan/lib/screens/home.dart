98679d3808b20f598ee9f50ff76109a5f3d0895a
Adria-Tisnawati-Aprilia <adriatisnawatiaprilia@gmail.com>
2022-05-15 10:03:45 +0700

    Mena

--- latihan/latihan/lib/screens/home.dart
+++ latihan/latihan/lib/screens/home.dart
@@ -1,4 +1,6 @@
 import'package:flutter/material.dart';
+import 'package:latihan/login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   const Home({ Key? key }) : super(key: key);
@@ -8,12 +10,25 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+
   @override
+  void initState(){
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
             title: Text("Home"),
-
+            actions: <Widget>[
+                IconButton(onPressed: ()async {
+                    SharedPreferences pref = await SharedPreferences.getInstance();
+                    await pref.clear();
+                    Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder:(context)=>LoginPage()
+                    ), (route) => false);
+                }, icon: Icon(Icons.logout)),
+            ],
         ),
         body: Center(child: Text("Home")),
     );
